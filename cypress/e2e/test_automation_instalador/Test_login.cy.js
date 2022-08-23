bac227b8c78c75d348366d6ba7da63c6358998b6
Juan Carlos Restrepo <juancarlos.restrepo@tecso.com.co>
2022-08-23 10:07:45 -0500

    prueba de login entre ambientes

--- cypress/e2e/test_automation_instalador/Test_login.cy.js
+++ cypress/e2e/test_automation_instalador/Test_login.cy.js
@@ -1,7 +1,7 @@
 //Para que funcionen los comandos de Cypress
 /// <reference types="cypress" />
 
-const cypress = require('cypress');
+//const cypress = require('cypress');
 
 require('cypress-xpath');
 
@@ -11,40 +11,13 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     return false
     });
 
-describe("Login spain instalador", () =>{
+ describe("Login spain instalador", () =>{
 
     it("Login spain", () =>{
         cy.log(`Running against ${Cypress.env('environment')} environment`)
-        cy.visit('/')
-        cy.log("Click en modulo del instalador")
-        cy.xpath("//i[@class='icon icon-ic_modulo_instalacion']").should("be.visible").click()
-        //cy.wait(1000)
-        cy.log("Despliegue de dropdown de países")
-        cy.xpath("//div[@class='mat-select-value'][contains(.,'España')]").should("be.visible").click()
-        //cy.wait(1000)
-        cy.log("Selección ESPAÑA")
-        cy.xpath("(//span[contains(.,'España')])[3]").should("be.visible").click()
-        //cy.wait(1000)
-
-        cy.log("Despliegue de dropdown de provider")
-        cy.xpath("(//div[contains(@class,'mat-select-value')])[2]").should("be.visible").click()
-        cy.log("Click en connection provider")
-        cy.xpath("//span[@class='m-login-input'][contains(.,'Cognito')]").should("be.visible").click()
-        //cy.wait(1000)
-        cy.log("Click en ENTRAR (al modulo del instalador)")
-        cy.xpath("//span[contains(text(),'Entrar')]").should("be.visible").click()
-
-        cy.log("Ingreso con usuario de Cognito")
-        cy.xpath("(//input[contains(@id,'signInFormUsername')])[2]").should("be.visible").type("smartis_automation")
-        cy.log("Ingreso con contraseña de Cognito")
-        cy.xpath("(//input[contains(@id,'signInFormPassword')])[2]").should("be.visible").type("Prosegur2022$")
-        //cy.wait(1000)
-        
-        cy.log("Click en signIn para Cognito (al modulo del instalador)")
-        cy.xpath("(//input[contains(@name,'signInSubmitButton')])[2]").should("be.visible").click()
-
+        cy.visit('/');
+        cy.login();
 
     })
-
 
 })//cierre de describe
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,6 +11,52 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
+
+require('cypress-xpath');
+
+Cypress.Commands.add('login', () => {
+  cy.log(`Iniciar sesión en ${Cypress.env('environment') ? Cypress.env('environment') : 'local'} environment`) 
+    
+    if (Cypress.env('environment') === 'pre') {
+      Cypress.env('user', Cypress.env('prelogUser'))
+    } else if (Cypress.env('environment') === 'prod') {
+      Cypress.env('user', Cypress.env('prodUser'))
+    } else if (Cypress.env ('environment') === 'dev') {
+      Cypress.env('user', Cypress.env('devUser'))
+    }
+
+    cy.visit('/login')
+
+    //cy.visit(Cypress.env('environment'))
+    cy.log("Click en modulo del instalador")
+    cy.xpath("//i[@class='icon icon-ic_modulo_instalacion']").should("be.visible").click()
+    //cy.wait(1000)
+    cy.log("Despliegue de dropdown de países")
+    cy.xpath("//div[@class='mat-select-value'][contains(.,'España')]").should("be.visible").click()
+    //cy.wait(1000)
+    cy.log("Selección ESPAÑA")
+    cy.xpath("(//span[contains(.,'España')])[3]").should("be.visible").click()
+    //cy.wait(1000)
+
+    cy.log("Despliegue de dropdown de provider")
+    cy.xpath("(//div[contains(@class,'mat-select-value')])[2]").should("be.visible").click()
+    cy.log("Click en connection provider")
+    cy.xpath("//span[@class='m-login-input'][contains(.,'Cognito')]").should("be.visible").click()
+    //cy.wait(1000)
+    cy.log("Click en ENTRAR (al modulo del instalador)")
+    cy.xpath("//span[contains(text(),'Entrar')]").should("be.visible").click()
+
+    cy.log("Ingreso con usuario de Cognito")
+    cy.xpath("(//input[contains(@id,'signInFormUsername')])[2]").should("be.visible").type(Cypress.env('user').username, { log: false })
+    cy.log("Ingreso con contraseña de Cognito")
+    cy.xpath("(//input[contains(@id,'signInFormPassword')])[2]").should("be.visible").type(Cypress.env('user').password, { log: false })
+    //cy.wait(1000)
+    
+    cy.log("Click en signIn para Cognito (al modulo del instalador)")
+    cy.xpath("(//input[contains(@name,'signInSubmitButton')])[2]").should("be.visible").click()
+
+  })
+
 //
 //
 // -- This is a child command --
